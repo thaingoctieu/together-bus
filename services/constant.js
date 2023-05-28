d22fdc9b966175888f89d46d11f537806d3b5018
tuantnb <tuantnb@vng.com.vn>
2023-05-28 23:33:40 +0700

    update endpoint

--- services/constant.js
+++ services/constant.js
@@ -1,2 +1,2 @@
-// export const API_URL = "http://www.tobus.site";
-export const API_URL = "http://192.168.1.9:3000";
+export const API_URL = "http://www.tobus.site";
+// export const API_URL = "http://192.168.1.9:3000";
